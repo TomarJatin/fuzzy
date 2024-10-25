ccf2a7a131b9049764ae4489e1754587fed87699
TomarJatin <jt24072002@gmail.com>
2024-10-26 00:58:45 +0530

    feat: project intialized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Scene from '../components/Scene'
+import Header from "@/components/LandingPage/Header";
+import Hero from "@/components/LandingPage/Hero";
+import SkyDive from "@/components/LandingPage/SkyDive";
+// import Scene from '../components/Scene'
 
 export default function Home() {
   return (
-    <div className="w-full h-screen">
-      <Scene />
+    <div>
+      <Header />
+      <Hero />
+      <SkyDive />
     </div>
-  )
+  );
 }
